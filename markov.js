6d08faa0691dea2cc9fc9b3b6b8477a665f2a495
Le Pham <phamtle724@gmail.com>
2022-08-16 16:10:55 -0700

    wrote getChains(), getText()

--- markov.js
+++ markov.js
@@ -1,3 +1,6 @@
+"use strict";
+const sample = require('lodash.sample');
+
 /** Textual markov chain generator. */
 
 
@@ -15,7 +18,7 @@ class MarkovMachine {
   /** Get markov chain: returns Map of Markov chains.
    *
    *  For text of "The cat in the hat.", chains will be:
-   * 
+   *
    *  {
    *   "The": ["cat"],
    *   "cat": ["in"],
@@ -23,13 +26,24 @@ class MarkovMachine {
    *   "the": ["hat."],
    *   "hat.": [null],
    *  }
-   * 
+   *
    * */
 
   getChains() {
-    // TODO: implement this!
-  }
+    //LOOP over the array of this.words and create a map
+    let chains = new Map();
 
+    for (let i = 0; i < this.words.length; i++) {
+
+      chains.set(this.words[i], [this.words[i + 1]]);
+
+      if (this.words[i + 1] === undefined) {
+        chains.set(this.words[i], null);
+      }
+    }
+    return chains;
+
+  }
 
   /** Return random text from chains, starting at the first word and continuing
    *  until it hits a null choice. */
@@ -40,5 +54,22 @@ class MarkovMachine {
     // - start at the first word in the input text
     // - find a random word from the following-words of that
     // - repeat until reaching the terminal null
+
+    //start with "The"
+    let resultText = [];
+    resultText.push(this.words[0])
+
+
+    //get random number that is smaller than the length of an array
+    // apply that random number to get a random word out of the array
+    //take the word taken from the array and use it as a key for the map to get its value
+    //do the same thing until null
+    let randomWord = ""
+    while (randomWord !== null) {
+      randomWord = _.sample(this.chains.get(resultText[resultText.length-1]))
+      resultText.push(randomWord);
+      }
+    return resultText.join(" ")
+
   }
 }
